63491ae147bef2c57167fe59162d582cc5ea4b8c
LEERHyun <35624406+LEERHyun@users.noreply.github.com>
2026-01-26 13:46:51 +0900

    Add files via upload

---
+++ arch/__init__.py
@@ -0,0 +1,10 @@
+from arch.model import HybridSplitNet
+import torch
+
+def create_model(ffn_expansion_factor=2,checkpoint_path=None,device = 'cuda:0'):
+
+    net = HybridSplitNet(ffn_expansion_factor=ffn_expansion_factor)
+    if checkpoint_path is not None:
+        checkpoint = torch.load(checkpoint_path, map_location=device, weights_only=True)
+        net.load_state_dict(checkpoint, strict=False)
+    return net
