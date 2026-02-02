1fb6f6b8a24852dba38af7de14f8ac0a6d095938
LEERHyun <35624406+LEERHyun@users.noreply.github.com>
2026-02-02 15:44:36 +0900

    Update __init__.py

--- arch/__init__.py
+++ arch/__init__.py
@@ -5,8 +5,9 @@ def create_model(ffn_expansion_factor=2,checkpoint_path=None,device = 'cuda:0'):
 
     net = HybridSplitNet(ffn_expansion_factor=ffn_expansion_factor)
     if checkpoint_path is not None:
-        checkpoint = torch.load(checkpoint_path, map_location=device, weights_only=True)
+        checkpoint = torch.load(checkpoint_path, weights_only=True)
         net.load_state_dict(checkpoint, strict=False)
         net.to(device)
 
     return net
+
